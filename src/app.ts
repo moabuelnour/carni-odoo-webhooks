eaa7912c2f519d77c89cfe52f73d9762c38cbfb2
Mohamed Abu El-Nour <thegreataint@metalfan.com>
2025-05-06 12:14:51 +0300

    feat: update scripts

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,17 @@ import { config } from "./config";
 import webhookRoutes from "./routes/webhook.routes";
 import dummyRoutes from "./routes/dummy.routes";
 import { connect } from "./db/mongodb";
+import rateLimit from "express-rate-limit";
 
 const app = express();
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests, please try again later.",
+  }),
+);
 app.use(express.json());
-
 app.use("/webhooks", webhookRoutes);
 app.use("/api/orders", dummyRoutes);
 
